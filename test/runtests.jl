ed709f22e6c3f058b3f83e1ddbc82b2b6da70919
Maxence Gollier <mgollier45@gmail.com>
2025-08-05 11:25:58 +0200

    add first tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,61 @@
 using ExactPenalty
-using Test
+using LinearOperators, RegularizedOptimization, RegularizedProblems, ShiftedProximalOperators, SolverCore, SparseMatricesCOO
+using LinearAlgebra, Test
+
+function read_instance(file::String; type = Float64, Hessian_modifier = H -> H)
+  lines = readlines(file)
+  data = Dict{String, Any}()
+  data["name"] = basename(file)
+
+  i = 1
+  while i <= length(lines)
+    key = strip(lines[i])
+    i += 1
+    if key == "tau" || key == "sigma"
+      data[key] = parse(type, strip(lines[i]))
+      i += 1
+    elseif key in ["nabla", "c"]
+      data[key] = parse.(type, split(strip(lines[i])))
+      i += 1
+    elseif key in ["J", "Q"]
+      matrix = []
+      while i <= length(lines) && !(strip(lines[i]) in ["tau", "sigma", "nabla", "c", "J", "Q"])
+        row = parse.(type, split(strip(lines[i])))
+        push!(matrix, row)
+        i += 1
+      end
+      data[key] = reduce(vcat, [reshape(r, 1, :) for r in matrix])
+
+    else
+      error("Unknown section: $key")
+    end
+  end
+  x0 = zeros(type, length(data["nabla"]))
+  model = R2NModel(Hessian_modifier(data["Q"]), data["nabla"], data["sigma"], x0)
+  c! = let c = data["c"] 
+    (b, x) -> 
+    begin
+      b .= c  
+    end
+  end
+  J! = let J = data["J"] 
+    (A, x) -> 
+    begin
+      A .= J  
+    end
+  end
+  h = ShiftedCompositeNormL2(data["tau"], c!, J!, SparseMatrixCOO(data["J"]), data["c"])
+  return RegularizedNLPModel(model, h)
+end
 
 for (root, dirs, files) in walkdir(@__DIR__)
-    for file in files
-        if isnothing(match(r"^test-.*\.jl$", file))
-            continue
-        end
-        title = titlecase(replace(splitext(file[6:end])[1], "-" => " "))
-        @testset "$title" begin
-            include(file)
-        end
+  for file in files
+    if isnothing(match(r"^test-.*\.jl$", file))
+      continue
     end
+    title = titlecase(replace(splitext(file[6:end])[1], "-" => " "))
+    @testset "$title" begin
+      include(file)
+    end
+  end
 end
---
+++ test/test-subsolvers.jl
@@ -0,0 +1,18 @@
+# Get instances 
+instances = "test/instances/"
+instances = filter(isfile, joinpath.(instances, readdir(instances)))
+
+# Get subsolvers
+solver_names = ["TRMoreSorensenLinOpSolver"]
+solvers = [TRMoreSorensenLinOpSolver]
+
+for (solver_name, solver) in zip(solver_names, solvers)
+  @testset "$solver_name" begin
+    for instance in instances
+      reg_nlp = read_instance(instance, type = Float64, Hessian_modifier = LinearOperator)
+      solver = eval(solver)(reg_nlp)
+      stats = RegularizedExecutionStats(reg_nlp)
+      @test @wrappedallocs(solve!(solver, reg_nlp, stats)) == 0 
+    end
+  end
+end
