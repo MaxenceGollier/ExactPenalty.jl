3f7f28f74302c5f346e93869d841601fd8bf98c3
Maxence Gollier <mgollier45@gmail.com>
2026-01-03 20:05:21 +0100

    add benchmark

---
+++ benchmark/benchmark-cutest.jl
@@ -0,0 +1,68 @@
+using CUTEst, ExactPenalty, NLPModelsModifiers, RegularizedOptimization, SolverBenchmark
+
+nmax = 300
+max_time = 100.0 #Each problem has 5 minutes.
+problem_names = CUTEst.select_sif_problems(min_con=1, max_var= nmax, only_equ_con=true, only_free_var=true)
+problem_list = (CUTestModel(name) for name in problem_names)
+
+imprecise_tol = 1e-3
+precise_tol = 1e-9
+
+solvers = Dict(
+  :l2penalty_exact_imprecise =>
+    nlp -> L2Penalty(
+      nlp,
+      verbose   = 0,
+      max_time  = max_time,
+      atol      = imprecise_tol,
+      rtol      = imprecise_tol,
+      subsolver = R2NSolver,
+    ),
+  :l2penalty_exact_precise =>
+    nlp -> L2Penalty(
+      nlp,
+      verbose   = 0,
+      max_time  = max_time,
+      atol      = precise_tol,
+      rtol      = precise_tol,
+      subsolver = R2NSolver,
+    ),
+  :l2penalty_lbfgs_imprecise =>
+    nlp -> L2Penalty(
+      LBFGSModel(nlp),
+      verbose   = 0,
+      max_time  = max_time,
+      atol      = imprecise_tol,
+      rtol      = imprecise_tol,
+      subsolver = R2NSolver,
+    ),
+  :l2penalty_lbfgs_precise =>
+    nlp -> L2Penalty(
+      LBFGSModel(nlp),
+      verbose   = 0,
+      max_time  = max_time,
+      atol      = precise_tol,
+      rtol      = precise_tol,
+      subsolver = R2NSolver,
+    ),
+  :l2penalty_r2_imprecise =>
+    nlp -> L2Penalty(
+      nlp,
+      max_time  = max_time,
+      verbose   = 0,
+      atol      = imprecise_tol,
+      rtol      = imprecise_tol,
+      subsolver = R2NSolver,
+    ),
+  :l2penalty_r2_precise =>
+    nlp -> L2Penalty(
+      nlp,
+      max_time  = max_time,
+      verbose   = 0,
+      atol      = precise_tol,
+      rtol      = precise_tol,
+      subsolver = R2NSolver,
+    ),
+)
+
+stats = bmark_solvers(solvers, problem_list, skipif=nlp -> nlp.meta.ncon ≥ nlp.meta.nvar)
