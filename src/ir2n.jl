66ca3665ba6c1bd03f3af016c71870a4e1177ebe
MaxenceGollier <mgollier45@gmail.com>
2026-06-02 11:33:10 -0400

    Revert ":robot: Format .jl files"
    This reverts commit 39caf66005b6811dca25eda7d70ea5d2fca41647.

--- src/ir2n.jl
+++ src/ir2n.jl
@@ -143,7 +143,7 @@ function SolverCore.solve!(
         :normx => "‖x‖",
         :norms => "‖s‖",
         :arrow => "PenaltyR2N",
-        :iter_type => "IterType",
+        :iter_type => "IterType"
       ),
       colsep = 1,
     )
@@ -159,7 +159,7 @@ function SolverCore.solve!(
         norm(xk),
         norm(s),
         (η2 ≤ ρk < Inf) ? '↘' : (ρk < η1 ? '↗' : '='),
-        "init",
+        "init"
       ],
       colsep = 1,
     )
@@ -238,8 +238,8 @@ function SolverCore.solve!(
           max_eval = max_eval - neval_obj(nlp),
           η1 = η1,
           η2 = η2,
-          γ = γ,
-          ρk = ρk,
+          γ  = γ,
+          ρk = ρk
         )
         fk, hk = stats.solver_specific[:smooth_obj], stats.solver_specific[:nonsmooth_obj]
         σk = stats.solver_specific[:sigma]
@@ -288,7 +288,7 @@ function SolverCore.solve!(
           norm(xk),
           norm(s),
           (η2 ≤ ρk < Inf) ? '↘' : (ρk < η1 ? '↗' : '='),
-          "ms",
+          "ms"
         ],
         colsep = 1,
       )
