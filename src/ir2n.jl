5b4edf8ec208b868d6a269f04c4e3377646b9098
MaxenceGollier <mgollier45@gmail.com>
2026-06-02 12:13:52 -0400

    add safeguard: respect theory

--- src/ir2n.jl
+++ src/ir2n.jl
@@ -219,7 +219,7 @@ function SolverCore.solve!(
 
     if η2 ≤ ρk < Inf
       σp = σk - more_sorensen_sigma!(solver.subsolver, solver.subpb, solver.substats; Δ = norm(s) * γ)
-      σk = isa(nlp, QuasiNewtonModel) ? max(σk/γ,σmin) : max(σp, σmin)
+      σk = isa(nlp, QuasiNewtonModel) ? max(σk/γ,σmin) : clamp(σp, max(σk/γ^10,σmin), σk/γ^(1/10))
     end
 
     if ρk < η1 || ρk == Inf
