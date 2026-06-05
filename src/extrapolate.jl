ecf85c037d336f6dc82c38530e484948e3b0b8df
MaxenceGollier <mgollier45@gmail.com>
2026-06-05 13:39:37 -0400

    document

--- src/extrapolate.jl
+++ src/extrapolate.jl
@@ -25,16 +25,17 @@ function extrapolate!(
   ∇fk = φ.data.c
   n, m = nlp.meta.nvar, nlp.meta.ncon
 
+  # Step 1. If the multipliers are smaller than the penalty parameter, the derivative w.r.t τ₁ is equal to 0.
   norm(y) < τ₁ && return false
 
-  # Update multipliers
+  # Step 2. Compute multipliers that are such that ‖y‖ < τ₁
   φ.data.σ = substats.solver_specific[:sigma]
   solve!(ms_solver, mk, ms_stats; accept_descent = false)
   get_primal_dual_sol!(s, y, ms_solver)
 
   ms_stats.status != :first_order && return false
 
-  # Step acceptance
+  # Step 3. Shift the model to the multipliers, check with an Armijo-condition
   xkn .= xk .+ s
   fkn, hkn = obj(nlp, xkn), h(xkn)
   mks = dot(∇fk, s) + ψ(s)
@@ -48,7 +49,7 @@ function extrapolate!(
   xk .= xkn
   shift!(mk, xk, y = y)
 
-  # Create problem
+  # Step 4. Construct u = [0  y]
   @. ms_solver.u1[1:n] = 0
   @. ms_solver.u1[(n+1):(n+m)] = y
 
@@ -60,6 +61,9 @@ function extrapolate!(
     ms_stats.solver_specific[:alpha],
   )
 
+  # Step 5. Solve
+  # [ H  Jᵀ ][z] = [u]
+  # [ J -αI ][z] = [u]
   solve_system!(ms_solver.workspace, ms_solver.u1)
   get_solution!(ms_solver.x1, ms_solver.workspace)
   status = get_status(ms_solver.workspace)
@@ -68,18 +72,17 @@ function extrapolate!(
 
   (status != :success || !check_inertia) && return false
 
+  # Step 6. Compute
+  # d[x]/dτ = τ₁[z]/uᵀz
+  # d[y]/dτ = τ₁[z]/uᵀz
   u1x1 = dot(ms_solver.u1, ms_solver.x1)
-
   abs(u1x1) < (norm(ms_solver.u1)*norm(ms_solver.x1)) * sqrt(eps(T)) && return false
-
   scale = τ₁ / dot(ms_solver.u1, ms_solver.x1)
-
-  scale == Inf && return false
   ms_solver.x1 .*= scale
 
-  solver.x .= xk .+ ms_solver.x1[1:n] #.* (τ₂ - τ₁)
-  solver.y .= y .+ ms_solver.x1[n+1:end] #.* (τ₂ - τ₁)
-
+  # Step 7. Update
+  solver.x .= xk .+ ms_solver.x1[1:n] .* (τ₂ - τ₁)
+  solver.y .= y .+ ms_solver.x1[n+1:end] .* (τ₂ - τ₁)
   norm(solver.y) > τ₂ && (solver.y .*= τ₂ / norm(solver.y))
   
   shift!(mk, solver.x, y = solver.y)
