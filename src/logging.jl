88ac8be76362e4472dc4c960bc28a398c2439628
MaxenceGollier <mgollier45@gmail.com>
2026-06-30 14:08:07 -0400

    continue logging update

--- src/logging.jl
+++ src/logging.jl
@@ -33,6 +33,12 @@ function introduction_message(solver::L2PenaltySolver, nlp::AbstractNLPModel)
   """
 end
 
+function introduction_message(solver::PenaltyR2NSolver, nlp::AbstractNLPModel, stats::GenericExecutionStats)
+  return @sprintf("
+        |  Solving subproblem minₓ f(x) + %-3.2e ‖c(x)‖₂ with tolerance %-3.2e...", 
+  stats.solver_specific[:tau], stats.solver_specific[:dual_ktol])
+end
+
 const W_ITER  = 6
 const W_LARGE = 16
 const W_MED   = 12
@@ -42,32 +48,76 @@ const FMT_OBJ   = "%+-16.7e"
 const FMT_MED   = "%-12.2e"
 const FMT_SMALL = "%+-8.1f"
 
-function separator()
-    repeat("-", textwidth(header_message()))
+function separator(;type = :outer_loop)
+  if type == :outer_loop
+    return repeat("-", textwidth(header_message(type = type)))
+  elseif type == :inner_loop
+    return "  | " * repeat("-", textwidth(header_message(type = type))-4)
+  end
 end
 
-function header_message()
-    @sprintf(
-        "%-*s%-*s%-*s%-*s%-*s%-*s%-*s",
+function header_message(; type = :outer_loop)
+  if type == :outer_loop
+    return @sprintf(
+        "%-*s%-*s%-*s%-*s%-*s%-*s%-*s%-*s",
         W_ITER,  "Iter",
         W_LARGE, "Objective",
-        W_MED,   "Primal_Feas",
-        W_MED,   "Dual_Feas",
-        W_SMALL, "log(τ)",
+        W_MED,   "pfeas",
+        W_MED,   "dfeas",
+        W_MED,   "τ",
+        W_MED,   "ptol",
+        W_MED,   "dtol",
         W_MED,   "‖x‖₂",
-        W_SMALL, "log(σ)",
     )
+  elseif type == :inner_loop
+    return @sprintf(
+        "  | %-*s%-*s%-*s%-*s%-*s%-*s%-*s%-*s",
+        W_ITER,  "Iter",
+        W_LARGE, "Objective",
+        W_MED,   "pfeas",
+        W_MED,   "dfeas",
+        W_MED,   "σ",
+        W_MED,   "ρ",
+        W_MED,   "‖x‖₂",
+        W_MED,   "‖s‖₂",
+    )
+  end
 end
 
-function log_iteration(solver, nlp, stats)
-    @sprintf(
-        "%-6d%-+16.7e%-12.2e%-12.2e%-+8.1f%-12.2e%-+8.1f",
-        stats.iter,
-        stats.objective,
-        stats.primal_feas,
-        stats.dual_feas,
-        log10(solver.substats.solver_specific[:tau]),
-        norm(solver.substats.solution),
-        log10(solver.substats.solver_specific[:sigma]),
+function log_iteration(solver, nlp, stats; type = :outer_loop)
+  if type == :outer_loop
+    return @sprintf(
+      "%-6d%-+16.7e%-12.2e%-12.2e%-12.2e%-12.2e%-12.2e%-12.2e",
+      stats.iter,
+      stats.objective,
+      stats.primal_feas,
+      stats.dual_feas,
+      solver.substats.solver_specific[:tau],
+      solver.substats.solver_specific[:primal_ktol],
+      solver.substats.solver_specific[:dual_ktol],
+      norm(solver.x),
     )
+  elseif type == :inner_loop
+    return @sprintf(
+      "  | %-6d%-+16.7e%-12.2e%-12.2e%-12.2e%-+12.2e%-12.2e%-12.2e",
+      stats.iter,
+      stats.objective,
+      stats.primal_feas,
+      stats.dual_feas,
+      stats.solver_specific[:sigma],
+      stats.solver_specific[:rho],
+      norm(solver.xk),
+      norm(solver.s),
+    )
+  end
+end
+
+function conclusion_message(solver, nlp, stats; type = :outer_loop)
+  if type == :outer_loop
+    return ""
+  elseif type == :inner_loop
+    return "  |
+        |  Subproblem solved with status $(stats.status), after $(stats.iter) iterations. 
+        |  Reached dfeas = $(stats.dual_feas)"
+  end
 end
